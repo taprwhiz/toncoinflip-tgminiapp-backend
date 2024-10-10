4725fbbb30763aec8387accbaca95b367a7b76e9
taprwhiz <sienkoalbert930607.as@gmail.com>
2024-10-11 02:42:37 +0900

    feat: add if condition of ton pkg list

--- prisma/wallet.ts
+++ prisma/wallet.ts
@@ -1,15 +1,18 @@
 import pkg from '@ton/ton';
-const {  WalletContractV4, TonClient4, internal, Address } = pkg;
-import {  mnemonicToPrivateKey } from 'ton-crypto';
-import {  getHttpV4Endpoint } from '@orbs-network/ton-access';
-// ------
+import { mnemonicToPrivateKey } from 'ton-crypto';
+import { getHttpV4Endpoint } from '@orbs-network/ton-access';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-const transfer = async (dest:any, wager:any) => {
+const { WalletContractV4, TonClient4, internal, Address } = pkg || {};
+if (!WalletContractV4) {
+    throw new Error("WalletContractV4 is not available. Check your imports.");
+}
+
+const transfer = async (dest: any, wager: any) => {
     const endpoint = await getHttpV4Endpoint({ network: 'testnet' });
     const client = new TonClient4({ endpoint });//,apiKey:"a69840dc49e6cd344adb3d936395e0004be71d05e6f56315215ca3e80e38dc4e"}
-    let mnemonics =(process.env.mnemonics || '').toString();
+    let mnemonics = (process.env.mnemonics || '').toString();
     let keyPair = await mnemonicToPrivateKey(mnemonics.split(' '));
 
     let workchain = 0;
@@ -19,7 +22,7 @@ const transfer = async (dest:any, wager:any) => {
     });
     let wallet = client.open(wallet_create);
     console.log('Wallet address: ', wallet.address);
-    console.log("pub key:", keyPair.publicKey )
+    console.log("pub key:", keyPair.publicKey)
     console.log("sec key:", keyPair.secretKey)
 
     let seqno = await wallet.getSeqno();
@@ -38,7 +41,7 @@ const transfer = async (dest:any, wager:any) => {
     });
 
     console.log("transfered");
-    console.log("to: " , dest);
+    console.log("to: ", dest);
     console.log("reward:", wager);
 }
 
